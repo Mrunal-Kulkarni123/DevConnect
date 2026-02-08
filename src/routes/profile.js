5bd5da9d4798b596e18e66592662b5c973d3b56b
Mrunal Kulkarni <kmrunal2910@gmail.com>
2026-02-08 21:17:58 +0530

    connection request API ans schema

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,10 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
-profileRouter.get("/profile", userAuth, async (req, res) => {
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const { validateEditProfileData } = require("../utils/validation");
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
@@ -11,4 +14,37 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
   }
 });
 
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Edit not allowed");
+    }
+    const loggedInUser = req.user;
+    Object.keys(req.body).forEach(
+      (field) => (loggedInUser[field] = req.body[field]),
+    );
+    await loggedInUser.save();
+    res.send(`${loggedInUser.firstName} Your Profile Was Updated Successfully`);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  const oldPassword = req.body.oldPassword;
+  const loggedInUser = req.user;
+  const isMatch = await bcrypt.compare(oldPassword, loggedInUser.password);
+  if (!isMatch) {
+    return res.send("Incorrect old password");
+  }
+  const newPassword = req.body.newPassword;
+  const isNewPasswordStrong = validator.isStrongPassword(newPassword);
+  if (!isNewPasswordStrong) {
+    res.send("Create a strong new password");
+  }
+  const passwordHash = await bcrypt.hash(newPassword, 10);
+  loggedInUser.password = passwordHash;
+  await loggedInUser.save();
+  res.send("Password Changed successfully");
+});
 module.exports = profileRouter;
--- src/routes/request.js
+++ src/routes/request.js
@@ -2,9 +2,48 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  console.log("Sending a connection request");
-  res.send("Connection request sent");
-});
+const connectionRequestModel = require("../models/connectionRequest");
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+      const toUserExistsInDB = await User.findById(toUserId);
+      if (!toUserExistsInDB) {
+        return res.send("User not found");
+      }
+
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).send("Invalid status type");
+      }
+
+      const existingConnectionRequest = await connectionRequestModel.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res.status(400).send("Connection request already exists");
+      }
+      const connectionRequest = new connectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.json({
+        message: "Connection request sent successfully",
+        data,
+      });
+    } catch (err) {
+      res.send(err.message);
+    }
+  },
+);
 
 module.exports = requestRouter;
